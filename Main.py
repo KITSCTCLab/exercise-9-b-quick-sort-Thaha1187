7a9cd1242b7b75b14dfc8400ee4d16f2645bc532
Thaha1187 <110010500+Thaha1187@users.noreply.github.com>
2022-10-10 21:42:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,10 +8,10 @@ def partition(array, low, high):
             i = i + 1
             (array[i], array[j]) = (array[j], array[i])
     (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 
+    return i + 1
 
 def quick_sort(array, low, high):
-    if low < high:
+    if low < high
         pi = partition(array, low, high)
         quick_sort(array, low, pi - 1)
         quick_sort(array, pi + 1, high)
